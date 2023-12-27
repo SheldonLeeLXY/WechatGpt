c6ea1912eda33f5fc58856ee35ec4a6a35930191
Sheldon <1003807869@qq.com>
2023-12-27 15:35:33 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+import openai
+import werobot
+from dotenv import load_dotenv
+
+OPENAI_API_KEY="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"
+WECHAT_TOKEN="123456"
+GPT_MODEL_VERSION=3
+MAX_TOKENS=100
+
+load_dotenv()
+
+robot = werobot.WeRoBot(token=WECHAT_TOKEN)
+
+openai.api_key = OPENAI_API_KEY
+
+max_tokens = MAX_TOKENS
+max_tokens = 100 if max_tokens is None else int(max_tokens)
+
+def get_gpt3_reply(text):
+    print("Calling text-davinci-003 API...")
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=text,
+        max_tokens=max_tokens,
+        temperature=0,
+    )
+    return response.choices[0].text.strip()
+
+def get_gpt3dot5_reply(text):
+    print("Calling gpt-3.5-turbo API...")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": text}
+        ],
+        max_tokens=max_tokens,
+        temperature=0.8
+    )
+    return response.choices[0].message.content.strip()
+
+@robot.text
+def hello_world(message):
+    if (GPT_MODEL_VERSION == "3"):
+        reply = get_gpt3_reply(message.content)
+    else:
+        reply = get_gpt3dot5_reply(message.content)
+
+    print(reply)
+
+    return reply
+
+# robot.config['HOST'] = '0.0.0.0'
+# port = os.getenv("PORT")
+# robot.config['PORT'] = 8888 if port is None else port
+# robot.run()
+
+### Integrate with Flask
+from flask import Flask
+from werobot.contrib.flask import make_view
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.add_url_rule(rule='/robot/', # WeRoBot 的绑定地址
+                endpoint='werobot', # Flask 的 endpoint
+                view_func=make_view(robot),
+                methods=['GET', 'POST'])
+
+port = os.getenv("PORT")
+port = 8888 if port is None else port
+app.run(host='0.0.0.0', port=port)
